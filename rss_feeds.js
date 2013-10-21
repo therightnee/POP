7b987302e48abc8767b0807c09b045476f3ed5b7
therightnee <therightnee@gmail.com>
2013-10-20 20:59:41 -0400

    first commit

---
+++ rss_feeds.js
@@ -0,0 +1,6 @@
+var feeds = [
+    {"header":"Top Stories" , "feed_url":"http://news.google.com/news?ned=us&topic=h&output=rss", "hex":"#2980b9"},
+    {"header":"World News" , "feed_url":"http://news.google.com/news?pz=1&cf=all&ned=us&hl=en&topic=w&output=rss", "hex":"#c0392b"},
+    {"header":"Sports" , "feed_url":"https://news.google.com/news/feeds?pz=1&cf=all&ned=us&hl=en&topic=s&output=rss", "hex":"#8e44ad"},
+    {"header":"Business" , "feed_url":"https://news.google.com/news/feeds?cf=all&ned=us&hl=en&topic=b&output=rss", "hex":"#16a085"}
+];
